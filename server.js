0ca7696e547bddd7beb9b2e03f4bf0ec8b6a36dd
Nicole Apsche <napsche@kgtiger.com>
2020-08-11 15:22:14 -0400

    connecting to mongo/heroku

--- server.js
+++ server.js
@@ -10,3 +10,36 @@ var cheerio = require("cheerio");
 
 // Require all models
 var db = require("./models");
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_215wrxkj:dbrillitjms5qsrqap1tk2ru60@ds215019.mlab.com:15019/heroku_215wrxkj', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_215wrxkj:dbrillitjms5qsrqap1tk2ru60@ds215019.mlab.com:15019/heroku_215wrxkj";
+
+// mongoose.connect(MONGODB_URI);
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
+
